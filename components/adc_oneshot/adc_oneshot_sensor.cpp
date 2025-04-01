89709928ef991c78c1ed5a846e228ee473f023f8
oxynatOr <98734567+oxynatOr@users.noreply.github.com>
2025-04-01 11:36:29 +0200

    rev: moved logging to Verbose

--- components/adc_oneshot/adc_oneshot_sensor.cpp
+++ components/adc_oneshot/adc_oneshot_sensor.cpp
@@ -146,11 +146,11 @@ float ADCOneshotSensor::sample()
   bool do_calibration1_chan0 = adc_calibration_init(ADC_UNIT_1, this->channel1_, this->attenuation_, &adc1_cali_chan0_handle);
   
   adc_oneshot_read(adc1_handle, this->channel1_, &adc_raw[0][0]);
-  ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, this->channel1_, adc_raw[0][0]);
+  ESP_LOGV(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, this->channel1_, adc_raw[0][0]);
   if (do_calibration1_chan0) {
       // Convert the ADC raw result into calibrated result
       ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan0_handle, adc_raw[0][0], &voltage[0][0]));
-      ESP_LOGI(TAG, "ADC%d Channel[%d] Calibrated Voltage: %d mV", ADC_UNIT_1 + 1, this->channel1_, voltage[0][0]);
+      ESP_LOGV(TAG, "ADC%d Channel[%d] Calibrated Voltage: %d mV", ADC_UNIT_1 + 1, this->channel1_, voltage[0][0]);
   }
   
 
@@ -179,7 +179,7 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
 
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
     if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+        ESP_LOGV(TAG, "calibration scheme version is %s", "Curve Fitting");
         adc_cali_curve_fitting_config_t cali_config = {
             .unit_id = unit,
             .chan = channel,
@@ -195,7 +195,7 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
 
 #if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
     if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+        ESP_LOGV(TAG, "calibration scheme version is %s", "Line Fitting");
         adc_cali_line_fitting_config_t cali_config = {
             .unit_id = unit,
             .atten = atten,
@@ -210,7 +210,7 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
 
     *out_handle = handle;
     if (ret == ESP_OK) {
-        ESP_LOGI(TAG, "Calibration Success");
+        ESP_LOGV(TAG, "Calibration Success");
     } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
         ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
     } else {
@@ -223,11 +223,11 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
 static void adc_calibration_deinit(adc_cali_handle_t handle)
 {
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
+    ESP_LOGV(TAG, "deregister %s calibration scheme", "Curve Fitting");
     ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
 
 #elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
+    ESP_LOGV(TAG, "deregister %s calibration scheme", "Line Fitting");
     ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
 #endif
 }
